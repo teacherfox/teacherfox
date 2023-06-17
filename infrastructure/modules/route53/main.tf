07614cbe448ef707f5309f5164675cffdd16fecb
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2023-06-17 12:47:49 +0300

    Create DKIM record

--- infrastructure/modules/route53/main.tf
+++ infrastructure/modules/route53/main.tf
@@ -9,10 +9,19 @@ resource "aws_route53_zone" "teacherfox" {
 resource "aws_route53_record" "zoho_verification" {
   count   = var.environment == "prod" ? 1 : 0
   zone_id = aws_route53_zone.teacherfox.zone_id
-  name    = "zoho_verification"
+  name    = ""
   type    = "TXT"
   ttl     = "86400"
   records = ["zoho-verification=zb82681392.zmverify.zoho.eu", "v=spf1 include:zoho.eu ~all"]
+}
+
+resource "aws_route53_record" "dkim" {
+  count   = var.environment == "prod" ? 1 : 0
+  zone_id = aws_route53_zone.teacherfox.zone_id
+  name    = "teacherfox._domainkey"
+  type    = "TXT"
+  ttl     = "86400"
+  records = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCLom4XYPxiyGVy/3skOMfvamHOOlnrQs/ZhKo1hVciQmJlGlG4U1HHGsFs8+9EchpaXYofFb35k7HvbpVn1274L7rIKtHZoQaAPkIQ9tMyVoE3eadIEhPZHvmEn6RBh5tScIU2OZS2pcwc5iufnPvUyOfSe/a+NJ9prkww2dXNbwIDAQAB"]
 }
 
 resource "aws_route53_record" "mail" {
