58ea8b881130712cf15a2ba5a1fc54f7bae75b04
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2023-07-15 10:16:39 +0300

    TF-43: Create dev ses

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -44,3 +44,16 @@ data "tfe_outputs" "prod_outputs" {
   organization = local.organization
   workspace = "${local.organization}-prod"
 }
+
+module "route53" {
+  source       = "../../modules/route53"
+  environment  = var.environment
+  organization = local.organization
+}
+
+module "ses" {
+  source      = "../../modules/ses"
+  domain = module.route53.domain_name
+  environment = var.environment
+  zone_id = module.route53.zone_id
+}
