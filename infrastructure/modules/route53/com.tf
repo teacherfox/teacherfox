550e64b0491bc71edb57583dd849584ce4fae03e
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2023-07-19 09:42:51 +0300

    TF-48: Create com extra entries

--- infrastructure/modules/route53/com.tf
+++ infrastructure/modules/route53/com.tf
@@ -10,7 +10,7 @@ resource "aws_route53_zone" "gr_zone" {
 
 resource "aws_route53_record" "teacherfox_com" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = ""
   type    = "CNAME"
   ttl     = "14400"
@@ -19,7 +19,7 @@ resource "aws_route53_record" "teacherfox_com" {
 
 resource "aws_route53_record" "teacherfox_com_a" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = ""
   type    = "A"
   ttl     = "14400"
@@ -28,7 +28,7 @@ resource "aws_route53_record" "teacherfox_com_a" {
 
 resource "aws_route53_record" "teacherfox_com_ipv6_extra" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = "ipv6"
   type    = "AAAA"
   ttl     = "14400"
@@ -37,16 +37,16 @@ resource "aws_route53_record" "teacherfox_com_ipv6_extra" {
 
 resource "aws_route53_record" "teacherfox_com_ipv6" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = ""
   type    = "AAAA"
   ttl     = "14400"
   records = ["2a02:c500:1:128::1"]
 }
 
-resource "aws_route53_record" "fast_mail" {
+resource "aws_route53_record" "teacherfox_com_fast_mail" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = ""
   type    = "TXT"
   ttl     = "14400"
@@ -55,7 +55,7 @@ resource "aws_route53_record" "fast_mail" {
 
 resource "aws_route53_record" "teacherfox_com_ftp" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = "ftp"
   type    = "CNAME"
   ttl     = "14400"
@@ -64,7 +64,7 @@ resource "aws_route53_record" "teacherfox_com_ftp" {
 
 resource "aws_route53_record" "teacherfox_com_webmail_a" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = "webmail"
   type    = "A"
   ttl     = "14400"
@@ -73,7 +73,7 @@ resource "aws_route53_record" "teacherfox_com_webmail_a" {
 
 resource "aws_route53_record" "teacherfox_com_webmail_aaaa" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = "webmail"
   type    = "AAAA"
   ttl     = "14400"
@@ -82,7 +82,7 @@ resource "aws_route53_record" "teacherfox_com_webmail_aaaa" {
 
 resource "aws_route53_record" "teacherfox_com_mail_a" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = "mail"
   type    = "A"
   ttl     = "14400"
@@ -92,7 +92,7 @@ resource "aws_route53_record" "teacherfox_com_mail_a" {
 
 resource "aws_route53_record" "teacherfox_com_mail_aaaa" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = "mail"
   type    = "AAAA"
   ttl     = "14400"
@@ -101,7 +101,7 @@ resource "aws_route53_record" "teacherfox_com_mail_aaaa" {
 
 resource "aws_route53_record" "teacherfox_com_mail" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = ""
   type    = "MX"
   ttl     = "300"
@@ -110,7 +110,7 @@ resource "aws_route53_record" "teacherfox_com_mail" {
 
 resource "aws_route53_record" "teacherfox_com_autoconfig" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = "autoconfig"
   type    = "CNAME"
   ttl     = "14400"
@@ -119,7 +119,7 @@ resource "aws_route53_record" "teacherfox_com_autoconfig" {
 
 resource "aws_route53_record" "teacherfox_com_acme_challenge" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = "_acme-challenge"
   type    = "TXT"
   ttl     = "14400"
@@ -128,7 +128,7 @@ resource "aws_route53_record" "teacherfox_com_acme_challenge" {
 
 resource "aws_route53_record" "teacherfox_com_autodiscover" {
   count   = var.environment == "prod" ? 1 : 0
-  zone_id = aws_route53_zone.com_zone.zone_id[0]
+  zone_id = aws_route53_zone.com_zone[0].zone_id
   name    = "_autodiscover._tcp"
   type    = "SRV"
   ttl     = "14400"
