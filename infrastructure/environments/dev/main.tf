5d14d266aa9cfba46b7c2d594ecef4a90f82567e
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2023-08-13 08:46:36 +0300

    Remove iam

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -58,12 +58,12 @@ data "tfe_outputs" "prod_outputs" {
 #  zone_id     = module.route53.zone_id
 #}
 
-module "iam_env" {
-  source                             = "../../modules/iam-env"
-  environment                        = var.environment
-  github_audience                    = data.tfe_outputs.prod_outputs.values.github_audience
-  github_openid_connect_provider_arn = data.tfe_outputs.prod_outputs.values.github_openid_connect_provider_arn
-  github_provider_url                = data.tfe_outputs.prod_outputs.values.github_provider_url
-  organization                       = local.organization
-#  ses_identity_arn                   = module.ses.identity_arn
-}
+#module "iam_env" {
+#  source                             = "../../modules/iam-env"
+#  environment                        = var.environment
+#  github_audience                    = data.tfe_outputs.prod_outputs.values.github_audience
+#  github_openid_connect_provider_arn = data.tfe_outputs.prod_outputs.values.github_openid_connect_provider_arn
+#  github_provider_url                = data.tfe_outputs.prod_outputs.values.github_provider_url
+#  organization                       = local.organization
+##  ses_identity_arn                   = module.ses.identity_arn
+#}
